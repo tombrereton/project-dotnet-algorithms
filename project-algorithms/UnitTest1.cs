c1b6f388986ae45aae5923842e06274550e385c1
tombrereton <tbrereton89@gmail.com>
2019-06-18 21:34:02 +0100

    Tidy up code for dijkstras

--- project-algorithms/UnitTest1.cs
+++ project-algorithms/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 
@@ -13,39 +14,25 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            /**
-             * List to hold distance from source to vertex u, dist[]
-             * List to hold if tight, mst[]
-             * List to hold predecessor pred[]
-             * Init dit[] with inifinity for all vertices except starting node which is 0
-
-             * While not all vertices in mst[]
-             * Pick vertex u, which is not in mst[] and has min dist[]
-             * Add to mst[]
-             * Update dist[] with
-             * loop through vertices vi adjacent to u and if dist[u] + edge(uv) < dist[v], dist[v] = dist[u] + edge(uv)
-             * 
-             */
-            var minSpanningTree = CalculateMinimumSpanningTree();
+            int[,] graph = new int[,]
+            {
+                {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+            };
+            var minSpanningTree = MinSpanningTree.CalculateMinimumSpanningTree(graph);
 
             Assert.That(minSpanningTree.Distance[8], Is.EqualTo(14));
         }
 
         [Test]
         public void ShouldShowNode1WithPredecessorNode0()
-        {
-            var minSpanningTree = CalculateMinimumSpanningTree();
-            Assert.That(minSpanningTree.Predecessor[1], Is.EqualTo(0));
-        }
-
-        [Test]
-        public void ShouldSHowNode4WithPredecessorNode5()
-        {
-            var minSpanningTree = CalculateMinimumSpanningTree();
-            Assert.That(minSpanningTree.Predecessor[4], Is.EqualTo(5));
-        }
-
-        private MinSpanningTree CalculateMinimumSpanningTree()
         {
             int[,] graph = new int[,]
             {
@@ -59,8 +46,63 @@ namespace Tests
                 {8, 11, 0, 0, 0, 0, 1, 0, 7},
                 {0, 0, 2, 0, 0, 0, 6, 7, 0}
             };
+            var minSpanningTree = MinSpanningTree.CalculateMinimumSpanningTree(graph);
+            Assert.That(minSpanningTree.Predecessor[1], Is.EqualTo(0));
+        }
 
-            var numberOfNodes = 9;
+        [Test]
+        public void ShouldSHowNode4WithPredecessorNode5()
+        {
+            int[,] graph = new int[,]
+            {
+                {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+            };
+            var minSpanningTree = MinSpanningTree.CalculateMinimumSpanningTree(graph);
+            Assert.That(minSpanningTree.Predecessor[4], Is.EqualTo(5));
+        }
+
+
+    }
+
+    internal class MinSpanningTree
+    {
+        public MinSpanningTree(int[] distance, int[] predecessor)
+        {
+            Distance = distance;
+            Predecessor = predecessor;
+        }
+
+        public int[] Distance { get; set; }
+        public int[] Predecessor { get; set; }
+        public static MinSpanningTree CalculateMinimumSpanningTree(int[,] graph)
+        {
+            /**
+             * Need:
+             * dist[]
+             * tight[]
+             * pred[]
+             *
+             *
+             * First set dist[A] = 0 and dist[i] = infinity for all i not A
+             * While not all nodes are tight
+             * Find the loose node i for which dist[i] is least
+             * make node i tight
+             * for each loose node j
+             * if( dist[i] + edge[i,j] < dist[j])
+             *   dist[j] = dist[i] + edge[i,j]
+             *   pred[j] = i
+             *
+             */
+
+            var numberOfNodes = (int)Math.Sqrt(graph.Length);
             var distance = new int[numberOfNodes];
             var predecessor = new int[numberOfNodes];
             var tightNodes = new bool[numberOfNodes];
@@ -91,7 +133,6 @@ namespace Tests
 
             return new MinSpanningTree(distance, predecessor);
         }
-
         private static bool IsPathThroughClosestNodeShorterToCurrentNode(int[] dist, int closestNode, int[,] graph, int currentNode)
         {
             return dist[closestNode] + graph[closestNode, currentNode] < dist[currentNode];
@@ -102,7 +143,7 @@ namespace Tests
             return !sptSet[currentNode] && graph[closestNode, currentNode] != 0 && dist[closestNode] != int.MaxValue;
         }
 
-        private int GetClosestNode(int[] dist, bool[] sptSet)
+        private static int GetClosestNode(int[] dist, bool[] sptSet)
         {
             var minDistance = int.MaxValue;
             var minIndex = -1;
@@ -118,17 +159,5 @@ namespace Tests
 
             return minIndex;
         }
-    }
-
-    internal class MinSpanningTree
-    {
-        public MinSpanningTree(int[] distance, int[] predecessor)
-        {
-            Distance = distance;
-            Predecessor = predecessor;
-        }
-
-        public int[] Distance { get; set; }
-        public int[] Predecessor { get; set; }
     }
 }
